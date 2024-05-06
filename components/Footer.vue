b66a310852e9876e0ad2abead0b752a5a2825fdb
Jehan-Mohamed <168306424+Jehan-Mohamed@users.noreply.github.com>
2024-05-06 19:38:35 +0300

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <v-card tile flat rounded="lg" class="pa-md-10 pa-5 text-center" color="surface">
         <h2 class="text-md font-weight-bold">Sheout shop</h2>
-        <p class="text-md mt-5 mb-2">E-commerce Site by</p>
+        <p class="text-md mt-5 mb-2">Sheout Site by</p>
         <p class="text-md mt-5 mb-2">Gehan Mohamed & Nourhan Alaa</p>
         <div class="text-center">
             <v-btn color="surface" class="mr-2" v-for="(b, i) in sm" :key="`sm${i}`" fab depressed>
